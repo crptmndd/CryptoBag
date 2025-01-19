f4a83596139aace0a042b0a4870f2ad4a7d909f1
NevermindGit <vadim.valeev.dev@gmail.com>
2025-01-19 18:58:24 +0300

    Initial Commit

---
+++ CryptoBag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoBag
+//
+//  Created by Vadim Valeev on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
